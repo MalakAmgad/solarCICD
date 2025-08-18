1ebfd32161907f77ed07108d9ca1c920f3ef245c
MalakAmgad <121722973+MalakAmgad@users.noreply.github.com>
2025-08-18 21:06:50 +0300

    comm

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "stagebucket13"
     key            = "stage-eks4/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
